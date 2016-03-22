6ee52d676d8f92e0acbfcf7a8c2cc667bbc41056
ryotakato <ryotakato4@gmail.com>
2016-03-22 22:48:05 +0900

    add test.html & sample test script

---
+++ test/test.life.js
@@ -0,0 +1,10 @@
+
+describe("test of test.life.js", function(){
+  it("aaa", function() {
+    assert.equal("aaa", "aaa");
+  });
+  it("bbb", function() {
+    assert.equal("bbb", "aaa");
+  });
+
+});
